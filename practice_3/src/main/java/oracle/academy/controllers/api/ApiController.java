11b85c61204eb7d7f5b26c59551d8dc2f25af93b
Oleg Tkachenko <tk4nko@ukr.net>
2016-03-12 18:13:17 +0200

    modified:   practice_3/src/main/java/oracle/academy/controllers/api/ApiController.java

--- practice_3/src/main/java/oracle/academy/controllers/api/ApiController.java
+++ practice_3/src/main/java/oracle/academy/controllers/api/ApiController.java
@@ -1,7 +1,34 @@
 package oracle.academy.controllers.api;
 
+import oracle.academy.model.User;
+import oracle.academy.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
 /**
  * Created by Home on 12.03.2016.
  */
+@Controller
 public class ApiController {
+
+    @Autowired
+    UserService userService;
+
+    @RequestMapping(value = "/api/users/get/{id}", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public User getUserById(@PathVariable("id") Long id) {
+        return userService.getById(id);
+    }
+
+    @RequestMapping(value = "/api/users/get/all", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public List<User> getAllUsers() {
+        return userService.getAll();
+    }
 }
